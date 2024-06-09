cd3bc63a0286a0217d4853421cc5486bbb67a807
danielbosa <danielbosa95@gmail.com>
2024-06-09 22:47:21 +0200

    Added projects with $this type

--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -23,6 +23,46 @@
         <img src="/images/placeholder.png" alt="{{$type->title}}">
         @endif
     </div>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Title</th>
+                <th scope="col">Slug</th>
+                <th scope="col" class="d-none d-md-table-cell">Created At</th>
+                <th scope="col" class="d-none d-md-table-cell">Update At</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($type->projects as $project)
+            <tr>
+                <td>{{$project->id}}</td>
+                <td>{{$project->title}}</td>
+                <td>{{$project->slug}}</td>
+                <td class="d-none d-md-table-cell">{{$project->created_at}}</td>
+                <td class="d-none d-md-table-cell">{{$project->updated_at}}</td>
+                <td>
+                    <a href="{{route('admin.projects.show', $project->slug)}}" title="Show" class="text-black px-2"><i class="fa-solid fa-eye"></i></a>
+                    <a href="{{route('admin.projects.edit', $project->slug)}}" title="Edit" class="text-black px-2"><i class="fa-solid fa-pen"></i></a>
+                    <form action="{{route('admin.projects.destroy', $project->slug)}}" method="POST" class="d-inline-block">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="delete-button border-0 bg-transparent"  data-item-title="{{ $project->title }}" data-item-id = "{{ $project->id }}">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+
+                    </form>
+
+
+                </td>
+            </tr>
+            @endforeach
+
+
+        </tbody>
+    </table>
 </section>
 @include('partials.modal-delete')
 @endsection
