9cd01b4a2faf7565d8f3c9b46a4ae84467ee499b
danielbosa <danielbosa95@gmail.com>
2024-06-09 22:22:04 +0200

    Seeding pivot table with shuffled existing elements

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
 use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
@@ -14,12 +16,18 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $types = Type::pluck('id');
+        $technologies = Technology::pluck('id');
         for($i = 0; $i < 10; $i++){
+            $shuffledTechnologies = $technologies->shuffle();
+            $shuffledTypes = $types->shuffle();
             $newProject = new Project();
             $newProject->title = $faker->sentence;
             $newProject->url = $faker->url;
             $newProject->slug = Project::generateSlug($newProject->title);
+            $newProject->type_id = $shuffledTypes->first();
             $newProject->save(); 
+            $newProject->technologies()->attach($shuffledTechnologies->take(2));
         }
         
     }
