e48df811ddae0e968cb79f04bb9799af9c9164a2
danielbosa <danielbosa95@gmail.com>
2024-06-09 09:52:34 +0200

    Technology: model setup

---
+++ app/Models/Technology.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Technology extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name','slug','icon'];
+
+    //Ho bisogno che lo slug sia unico per ogni tecnologia, quindi ciclo, conto e in base al count definisco slug
+    public static function generateSlug($name)
+    {
+        $slug = Str::slug($name, '-');
+        $count = 1;
+        while(Project::where('slug', $slug)->first()){
+            $slug = Str::slug($name, '-') . "-{$count}";
+            $count++;
+        }
+        return $slug;
+    }
+}
