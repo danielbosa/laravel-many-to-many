623cd61b8d250df5e1232b109328abf36e71b1fd
danielbosa <danielbosa95@gmail.com>
2024-06-09 16:23:35 +0200

    Fix

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug']);
     Route::resource('types', TypeController::class)->parameters(['types' => 'type:slug']);
-    Route::resource('types', TechnologyController::class)->parameters(['types' => 'type:slug']);
+    Route::resource('technologies', TechnologyController::class)->parameters(['technologies' => 'technologies:slug']);
 });
 
 
